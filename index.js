99a2c14d929b5fd8208fd6b61a3a360789700070
kai zhu <kaizhu256@gmail.com>
2016-08-27 19:57:45 +0800

    cull path module

--- index.js
+++ index.js
@@ -31,7 +31,7 @@
 */
 (function (local) {
     'use strict';
-    var nop, require;
+    var nop, pathDirname, require;
 
 
 
@@ -44,11 +44,22 @@
             return;
         };
         // jslint-hack
-        nop(require);
+        nop(pathDirname, require);
         // init global
         local.global = local.modeJs === 'browser'
             ? window
             : global;
+        pathDirname = function (path) {
+            var dir = (/^(.+\/(?!$)|\/)?((?:.+?)?(\.[^.]*)?)$/).exec(path)[1] || '';
+            return !dir
+                // No dirname
+                ? '.'
+                : dir.length === 1
+                // It is just a slash or a drive letter with a slash
+                ? dir
+                // It is a full dirname, strip trailing slash
+                : dir.slice(0, dir.length - 1);
+        };
         require = function (key) {
             switch (key) {
             case 'async':
@@ -119,7 +130,6 @@
 var fs = require('fs')
     , mkdirp = require('mkdirp')
     , async = require('async')
-    , path = require('path')
     , storage = {}
     ;
 
@@ -177,7 +187,7 @@ storage.crashSafeWriteFile = function (filename, data, cb) {
         , tempFilename = filename + '~';
 
     async.waterfall([
-        async.apply(storage.flushToStorage, { filename: path.dirname(filename), isDir: true })
+        async.apply(storage.flushToStorage, { filename: pathDirname(filename), isDir: true })
     , function (cb) {
             storage.exists(filename, function (exists) {
                 if (exists) {
@@ -194,7 +204,7 @@ storage.crashSafeWriteFile = function (filename, data, cb) {
     , function (cb) {
             storage.rename(tempFilename, filename, function (err) { return cb(err); });
         }
-    , async.apply(storage.flushToStorage, { filename: path.dirname(filename), isDir: true })
+    , async.apply(storage.flushToStorage, { filename: pathDirname(filename), isDir: true })
     ], function (err) { return callback(err); })
 };
 
@@ -243,186 +253,7 @@ module.exports = storage;
 (function(e){if("function"==typeof bootstrap)bootstrap("nedb",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeNedb=e}else"undefined"!=typeof window?window.Nedb=e():global.Nedb=e()})(function(){var define,ses,bootstrap,module,exports;
 return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return local[t] = i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({
 
-2:[function(require,module,exports){
-var process=require("__browserify_process");function filter (xs, fn) {
-    var res = [];
-    for (var i = 0; i < xs.length; i++) {
-        if (fn(xs[i], i, xs)) res.push(xs[i]);
-    }
-    return res;
-}
-
-// resolves . and .. elements in a path array with directory names there
-// must be no slashes, empty elements, or device names (c:\) in the array
-// (so also no leading and trailing slashes - it does not distinguish
-// relative and absolute paths)
-function normalizeArray(parts, allowAboveRoot) {
-  // if the path tries to go above the root, `up` ends up > 0
-  var up = 0;
-  for (var i = parts.length; i >= 0; i--) {
-    var last = parts[i];
-    if (last == '.') {
-      parts.splice(i, 1);
-    } else if (last === '..') {
-      parts.splice(i, 1);
-      up++;
-    } else if (up) {
-      parts.splice(i, 1);
-      up--;
-    }
-  }
-
-  // if the path is allowed to go above the root, restore leading ..s
-  if (allowAboveRoot) {
-    for (; up--; up) {
-      parts.unshift('..');
-    }
-  }
-
-  return parts;
-}
-
-// Regex to split a filename into [*, dir, basename, ext]
-// posix version
-var splitPathRe = /^(.+\/(?!$)|\/)?((?:.+?)?(\.[^.]*)?)$/;
-
-// path.resolve([from ...], to)
-// posix version
-exports.resolve = function() {
-var resolvedPath = '',
-    resolvedAbsolute = false;
-
-for (var i = arguments.length; i >= -1 && !resolvedAbsolute; i--) {
-  var path = (i >= 0)
-      ? arguments[i]
-      : process.cwd();
-
-  // Skip empty and invalid entries
-  if (typeof path !== 'string' || !path) {
-    continue;
-  }
-
-  resolvedPath = path + '/' + resolvedPath;
-  resolvedAbsolute = path.charAt(0) === '/';
-}
-
-// At this point the path should be resolved to a full absolute path, but
-// handle relative paths to be safe (might happen when process.cwd() fails)
-
-// Normalize the path
-resolvedPath = normalizeArray(filter(resolvedPath.split('/'), function(p) {
-    return !!p;
-  }), !resolvedAbsolute).join('/');
-
-  return ((resolvedAbsolute ? '/' : '') + resolvedPath) || '.';
-};
-
-// path.normalize(path)
-// posix version
-exports.normalize = function(path) {
-var isAbsolute = path.charAt(0) === '/',
-    trailingSlash = path.slice(-1) === '/';
-
-// Normalize the path
-path = normalizeArray(filter(path.split('/'), function(p) {
-    return !!p;
-  }), !isAbsolute).join('/');
-
-  if (!path && !isAbsolute) {
-    path = '.';
-  }
-  if (path && trailingSlash) {
-    path += '/';
-  }
-
-  return (isAbsolute ? '/' : '') + path;
-};
-
-
-// posix version
-exports.join = function() {
-  var paths = Array.prototype.slice.call(arguments, 0);
-  return exports.normalize(filter(paths, function(p, index) {
-    return p && typeof p === 'string';
-  }).join('/'));
-};
-
-
-exports.dirname = function(path) {
-  var dir = splitPathRe.exec(path)[1] || '';
-  var isWindows = false;
-  if (!dir) {
-    // No dirname
-    return '.';
-  } else if (dir.length === 1 ||
-      (isWindows && dir.length <= 3 && dir.charAt(1) === ':')) {
-    // It is just a slash or a drive letter with a slash
-    return dir;
-  } else {
-    // It is a full dirname, strip trailing slash
-    return dir.substring(0, dir.length - 1);
-  }
-};
-
-
-exports.basename = function(path, ext) {
-  var f = splitPathRe.exec(path)[2] || '';
-  // TODO: make this comparison case-insensitive on windows?
-  if (ext && f.substr(-1 * ext.length) === ext) {
-    f = f.substr(0, f.length - ext.length);
-  }
-  return f;
-};
-
-
-exports.extname = function(path) {
-  return splitPathRe.exec(path)[3] || '';
-};
-
-exports.relative = function(from, to) {
-  from = exports.resolve(from).substr(1);
-  to = exports.resolve(to).substr(1);
-
-  function trim(arr) {
-    var start = 0;
-    for (; start < arr.length; start++) {
-      if (arr[start] !== '') break;
-    }
-
-    var end = arr.length - 1;
-    for (; end >= 0; end--) {
-      if (arr[end] !== '') break;
-    }
-
-    if (start > end) return [];
-    return arr.slice(start, end - start + 1);
-  }
-
-  var fromParts = trim(from.split('/'));
-  var toParts = trim(to.split('/'));
-
-  var length = Math.min(fromParts.length, toParts.length);
-  var samePartsLength = length;
-  for (var i = 0; i < length; i++) {
-    if (fromParts[i] !== toParts[i]) {
-      samePartsLength = i;
-      break;
-    }
-  }
-
-  var outputParts = [];
-  for (var i = samePartsLength; i < fromParts.length; i++) {
-    outputParts.push('..');
-  }
-
-  outputParts = outputParts.concat(toParts.slice(samePartsLength));
-
-  return outputParts.join('/');
-};
-
-exports.sep = '/';
-
-},{"__browserify_process":4}],3:[function(require,module,exports){
+3:[function(require,module,exports){
 
 exports.isDate = function(obj){return Object.prototype.toString.call(obj) === '[object Date]'};
 exports.isRegExp = function(obj){return Object.prototype.toString.call(obj) === '[object RegExp]'};
@@ -3024,7 +2855,6 @@ var process=require("__browserify_process");/**
  */
 
 var storage = local.storage = local.storage || require('./storage')
-  , path = require('path')
   , model = require('./model')
   , async = require('async')
   , customUtils = require('./customUtils')
@@ -3240,7 +3070,7 @@ Persistence.prototype.loadDatabase = function (cb) {
 
   async.waterfall([
     function (cb) {
-      Persistence.ensureDirectoryExists(path.dirname(self.filename), function (err) {
+      Persistence.ensureDirectoryExists(pathDirname(self.filename), function (err) {
         storage.ensureDatafileIntegrity(self.filename, function (err) {
           storage.readFile(self.filename, 'utf8', function (err, rawData) {
             if (err) { return cb(err); }
@@ -3281,7 +3111,7 @@ Persistence.prototype.loadDatabase = function (cb) {
 // Interface
 module.exports = Persistence;
 
-},{"./customUtils":6,"./indexes":9,"./model":10,"./storage":12,"__browserify_process":4,"async":13,"path":2}],12:[function(require,module,exports){
+},{"./customUtils":6,"./indexes":9,"./model":10,"./storage":12,"__browserify_process":4,"async":13}],12:[function(require,module,exports){
 /**
  * Way data is stored for this database
  * For a Node.js/Node Webkit database it's the file system
